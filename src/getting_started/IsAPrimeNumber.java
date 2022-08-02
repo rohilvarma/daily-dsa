a924b1541ac8ec2baa47f196c6cac04b9cab847b
1N7R0V3R7 <rohilvarma96@gmail.com>
2022-08-03 02:17:25 +0530

    first commit

---
+++ src/getting_started/IsAPrimeNumber.java
@@ -0,0 +1,22 @@
+package getting_started;
+
+import java.util.*;
+
+public class IsAPrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0) {
+            int n = sc.nextInt();
+            String res = "prime";
+            for(int i=2 ; i*i<=n ; i++) {
+                if(n%i==0) {
+                    res = "not prime";
+                    break;
+                }
+            }
+            System.out.println(res);
+        }
+        sc.close();
+    }
+}
